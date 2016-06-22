fef59a32eb83e6fca9175a6e2d7a5e7eeec38140
Joe Wise <joedavidwise@gmail.com>
2016-06-22 10:16:32 -0700

    Added all major keys and their octaves as an array of arrays. Added initial code for handling joystick input. Abandoned slide functionality.

--- arcade_synth.ino
+++ arcade_synth.ino
@@ -1,56 +1,61 @@
 #include "pitches.h"
 
 // Set pin numbers using constants
-const int LED_1 = 2;
+const int LED_1    = 2;
 const int BUTTON_1 = 3;
-const int LED_2 = 4;
+const int LED_2    = 4;
 const int BUTTON_2 = 5;
-const int LED_3 = 6;
+const int LED_3    = 6;
 const int BUTTON_3 = 7;
-const int LED_4 = 8;
+const int LED_4    = 8;
 const int BUTTON_4 = 9;
-const int LED_5 = 26;
+const int LED_5    = 26;
 const int BUTTON_5 = 27;
-const int LED_6 = 28;
+const int LED_6    = 28;
 const int BUTTON_6 = 29;
-const int LED_7 = 30;
+const int LED_7    = 30;
 const int BUTTON_7 = 31;
-const int LED_8 = 32;
+const int LED_8    = 32;
 const int BUTTON_8 = 33;
+
+const int OCTAVE_DOWN = 10;
+const int OCTAVE_UP   = 11;
+const int KEY_UP      = 12;
+const int KEY_DOWN    = 13;
+
 const int SPEAKER_PIN = 14;
 
 int buttons[] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BUTTON_5, BUTTON_6, BUTTON_7, BUTTON_8};
 int leds[] = {LED_1, LED_2, LED_3, LED_4, LED_5, LED_6, LED_7, LED_8};
+int joystick[] = {OCTAVE_DOWN, OCTAVE_UP, KEY_UP, KEY_DOWN};
 
-int const key_c4[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5};
-
-int curr_octave = 4;
-char curr_key = 'c';
+int curr_octave = 3;
+char curr_key = 0;
 bool arpeggio = true;
-bool slide = false;
-
-// Globals for slide functionality
-int curr_button = -1;
-int prev_button = -1;
-    
 
 void setup() {
-// Set all Button pins as inputs
+  // Set all Button pins as inputs
   int i;
   for(i = 0; i < 8; i = i + 1){
     pinMode(buttons[i], INPUT);
   }
 
-// Set all LED pins as outputs and turn the LEDs on
+  // Set all LED pins as outputs and turn the LEDs on
   int j;
   for(j = 0; j < 8; j = j + 1){
     pinMode(leds[j], OUTPUT);
     digitalWrite(leds[j], HIGH);
   }
 
-  // the speaker pin as an output
+  // Set the speaker pin as an output
   pinMode(SPEAKER_PIN, OUTPUT);
 
+  // Set the joystick pins as inputs
+  pinMode(OCTAVE_DOWN, INPUT);
+  pinMode(OCTAVE_UP, INPUT);
+  pinMode(KEY_DOWN, INPUT);
+  pinMode(OCTAVE_UP, INPUT);
+  
   Serial.begin(9600);  // Sets the baud rate for serial transmission 
   // Send these messages to PC only once 
   Serial.println("Serial interface between PC and chipKIT");
@@ -60,6 +65,7 @@ void setup() {
 //    Serial.println(key_c4[k]);
 //  }
 
+  Serial.println(c_maj_keys[3][5]);
 }
  
 void loop(){
@@ -101,7 +107,7 @@ void loop(){
 
       digitalWrite(leds[last_true], LOW);
       
-      // Check if any other buttons are being press, if so break.
+      // Check if any other buttons are being pressed, if so break.
       int j;
       for(j = 0; j < 8; j = j + 1){
         if (j != last_true && digitalRead(buttons[j]) == HIGH) {
@@ -110,114 +116,38 @@ void loop(){
           goto multiple_notes_2;
         }
       }
+
+      // Check if any joystick buttons are being pressed
+      int k;
+      for(k = 0; k < 4; k = k + 1){
+        if (digitalRead(joystick[k]) == LOW){
+          joystick_input(k);
+        }
+      }
+      
       delay(10);
     }
     multiple_notes_2:
     
     for(i = 0; i < 8; i = i + 1){
       if (curr_buttons[i]) {
-//        Serial.print("multiple buttons pressed: "); Serial.println(i);
+        // Serial.print("multiple buttons pressed: "); Serial.println(i);
         tone(SPEAKER_PIN, determine_note(i));
         digitalWrite(leds[i], LOW);
         delay(32);
         digitalWrite(leds[i], HIGH);
       }
-    }  
-  }
-
-  else if(slide){
-    
-    int i;
-    int num_false = 0;
-    
- // Determine which buttons are being pressed
-    for(i = 0; i < 8; i = i + 1){
-      if (digitalRead(buttons[i]) == HIGH) {
-        curr_button = i;
-      }
-      else{
-        num_false = num_false + 1;
-      }
     }
-
-    Serial.print("curr_button: "); Serial.println(curr_button);
-
-    // If no buttons are being pressed, turn off the sound
-    if (num_false == 8){
-      noTone(SPEAKER_PIN);
-      curr_button = -1;
-      Serial.println("no buttons being pressed");
-    }
-
-    if (curr_button != -1 && prev_button != -1 && curr_button != prev_button) {
-      int initial_note = determine_note(prev_button);
-      int end_note = determine_note(curr_button);
-      int in_between = initial_note;
-
-      Serial.print("initial_note: "); Serial.println(initial_note);
-      Serial.print("end_note: "); Serial.println(end_note);
-      
-      if(initial_note > end_note){
-        Serial.println("initial_note > end_note");
-        while(in_between >= end_note){
-          in_between = in_between - 10;
-          tone(SPEAKER_PIN, in_between);
-          delay(10);
-          Serial.print("in_between: "); Serial.println(in_between);
-        }
-      }
-      else{
-        Serial.println("initial_note < end_note");
-        while(in_between <= end_note){
-          in_between = in_between + 10;
-          tone(SPEAKER_PIN, in_between);
-          delay(10);
-          Serial.print("in_between: "); Serial.println(in_between);
-        }
-      }
-    }
-
-    if(curr_button != -1){
-      tone(SPEAKER_PIN, determine_note(curr_button));
-      Serial.print("play tone from curr_button: "); Serial.println(curr_button);
-    }
-    
-    while (true){
-      if (curr_button == -1 || digitalRead(buttons[curr_button]) != HIGH){
-        noTone(SPEAKER_PIN);
-        Serial.println("stop playing tone");
-        curr_button = -1;
-        break;
-      }
-      
-      int j;
-      for(j = 0; j < 8; j = j + 1){
-        if (j != curr_button && digitalRead(buttons[j]) == HIGH) {
-          prev_button = curr_button;
-          Serial.print("prev_button: "); Serial.println(prev_button);
-          
-          curr_button = j;
-          Serial.print("second button pressed: "); Serial.println(curr_button);
-          
-          goto multiple_notes;
-        }
-      }
-      delay(10);
-    }
-    multiple_notes:
-      int pointless;
-//      delay(1000);
-
   }
   else {
     int i;
     for(i = 0; i < 8; i = i + 1){
       if (digitalRead(buttons[i]) == HIGH) {
         Serial.print("i: "); Serial.println(i);
+        
         tone(SPEAKER_PIN, determine_note(i));
         digitalWrite(leds[i], LOW);
-
-        tone(SPEAKER_PIN, determine_note(i));
+        
         while (digitalRead(buttons[i]) == HIGH) {
 //        hold note
 //          bool new_note = false;
@@ -232,6 +162,14 @@ void loop(){
 //          if(new_note){
 //            break;
 //          }
+        
+          // Check if any joystick buttons are being pressed
+          int k;
+          for(k = 0; k < 4; k = k + 1){
+            if (digitalRead(joystick[k]) == LOW){
+              joystick_input(k);
+            }
+          }
         }
 
         //Once the button isn't low, stop the tone and turn the LED on
@@ -240,60 +178,101 @@ void loop(){
       }   
     }    
   }
+ 
+  // Check if any joystick buttons are being pressed
+  int k;
+  for(k = 0; k < 4; k = k + 1){
+    if (digitalRead(joystick[k]) == LOW){
+      joystick_input(k);
+    }
+  }
+}
+
+void joystick_input(int type){
+  Serial.print("octave was: "); Serial.println(curr_octave);
+  Serial.print("key was: "); Serial.println(curr_key);
+  
+  // octave down
+  if(type == 1){
+    if(curr_octave == 0){
+      curr_octave = 7;
+    }
+    else{
+      curr_octave = curr_octave - 1;
+    }
+  }
+  
+  // octave up
+  else if(type == 2){
+    if(curr_octave == 7){
+      curr_octave = 0;
+    }
+    else{
+      curr_octave = curr_octave + 1;
+    }    
+  }
+  
+  // key up
+  else if(type == 3){
+    if(curr_key == 11){
+      curr_key = 0;
+    }
+    else{
+      curr_key = curr_key + 1;
+    }    
+  }
+  
+  // key down
+  else if(type == 4){
+    if(curr_key == 0){
+      curr_key = 11;
+    }
+    else{
+      curr_key = curr_key - 1;
+    }    
+  }
+
+  Serial.print("octave is now: "); Serial.println(curr_octave);
+  Serial.print("key is now: "); Serial.println(curr_key);
 }
 
 int determine_note(int button_num){
   switch (curr_key) {
-    case 'c':
-      return get_note_c(button_num);
+    case 0:
+      return c_maj_keys[curr_octave][button_num];
       break;
-    case 'd':
-      //do something when var equals 2
-      break;
-    case 'e':
-      //do something when var equals 2
-      break;
-    case 'f':
-      //do something when var equals 2
-      break;
-    case 'g':
-      //do something when var equals 2
-      break;
-    case 'a':
-      //do something when var equals 2
-      break;
-    case 'b':
-      //do something when var equals 2
-      break;
-  }
-}
-
-int get_note_c(int button_num) {
-  switch (curr_octave) {
     case 1:
-      //do something when curr_octave equals 1
+      return cs_maj_keys[curr_octave][button_num];
       break;
     case 2:
-      //do something when var equals 2
+      return d_maj_keys[curr_octave][button_num];
       break;
     case 3:
-      //do something when var equals 2
+      return ds_maj_keys[curr_octave][button_num];
       break;
     case 4:
-      return key_c4[button_num];
+      return e_maj_keys[curr_octave][button_num];
       break;
     case 5:
-      //do something when var equals 2
+      return f_maj_keys[curr_octave][button_num];
       break;
     case 6:
-      //do something when var equals 2
+      return fs_maj_keys[curr_octave][button_num];
       break;
     case 7:
-      //do something when var equals 2
+      return g_maj_keys[curr_octave][button_num];
       break;
     case 8:
-      //do something when var equals 2
-      break;      
+      return gs_maj_keys[curr_octave][button_num];
+      break;
+    case 9:
+      return a_maj_keys[curr_octave][button_num];
+      break;
+    case 10:
+      return as_maj_keys[curr_octave][button_num];
+      break;
+    case 11:
+      return b_maj_keys[curr_octave][button_num];
+      break;
   }
 }
-
